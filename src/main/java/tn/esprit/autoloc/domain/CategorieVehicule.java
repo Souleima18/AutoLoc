0fd2611917becf3a13e96f5621386bbe699524a7
Souleima18 <souleimab945@gmail.com>
2026-09-25 20:09:05 +0100

    les entités

---
+++ src/main/java/tn/esprit/autoloc/domain/CategorieVehicule.java
@@ -0,0 +1,4 @@
+package tn.esprit.autoloc.domain;
+public enum CategorieVehicule {
+    CITADINE, BERLINE, SUV, UTILITAIRE
+}
---
+++ src/main/java/tn/esprit/autoloc/domain/StatutVehicule.java
@@ -0,0 +1,4 @@
+package tn.esprit.autoloc.domain;
+public enum StatutVehicule {
+    DISPONIBLE, LOUE, MAINTENANCE
+}
